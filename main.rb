1fd72769f9b4f3c9d2d43ab8fee7698b33423a23
Jakub Bujny <jakub.bujny@ac-project.net>
2017-11-27 23:11:50 +0100

    Working youtube comments aggregation and finding matches - TODO bigger data set as input

--- main.rb
+++ main.rb
@@ -1,22 +1,21 @@
-# Sample Ruby code for user authorization
 require 'google/apis/youtube_v3'
 require 'fileutils'
 require 'json'
 
-# Initialize the API
 service = Google::Apis::YoutubeV3::YouTubeService.new
 service.client_options.application_name = "youtube-similarity-finder"
 service.key = ENV['YOUTUBE_API_KEY']
 
-# Sample ruby code for channels.list
 
-def channels_list_by_username(service, part, **params)
-  response = service.list_channels(part, params).to_json
-  item = JSON.parse(response).fetch("items")[0]
-
-  puts ("This channel's ID is #{item.fetch("id")}. " +
-      "Its title is '#{item.fetch("snippet").fetch("title")}', and it has " +
-      "#{item.fetch("statistics").fetch("viewCount")} views.")
-end
-
-channels_list_by_username(service, 'snippet,contentDetails,statistics', for_username: 'GoogleDevelopers')
+sourceVideoId = "m9UB9B0CSok"
+channelsIdsFromMovieComments =  JSON.parse(service.list_comment_threads("snippet", video_id: sourceVideoId,max_results: 100).to_json)["items"].map{|response| response["snippet"]["topLevelComment"]["snippet"]["authorChannelId"]["value"]}
+favorites = channelsIdsFromMovieComments.map{|channelId| JSON.parse(service.list_channels("contentDetails", id:channelId).to_json)["items"].first["contentDetails"]["relatedPlaylists"]["favorites"]}.select{|value| !value.nil?}
+videosList = favorites.map {|favoritePlaylistId| JSON.parse(service.list_playlist_items("contentDetails", playlist_id: favoritePlaylistId).to_json)["items"]}.flatten.map{|video| video["contentDetails"]["videoId"]}
+puts videosList.reduce(Hash.new(0)) {|acc,el|
+  if acc.has_key? el
+    acc[el] += 1
+  else
+    acc[el] = 1
+  end
+  acc
+}.select{|k,v| v > 1}.sort_by {|k,v| v}.reverse
